d701def81f716c52b2bc8c7629793803963c33a0
Anton Komrachkov <a.komrachkov@gmail.com>
2022-05-21 13:30:06 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+
+    public Randoms(int min, int max) {
+        //...
+    }
+
+    //...
+}
