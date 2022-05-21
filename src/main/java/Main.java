2ed2a184436f71858bdd3371dec072d348e20881
Anton Komrachkov <a.komrachkov@gmail.com>
2022-05-21 13:58:02 +0300

    Task done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import ru.netology.Randoms;
 
 public class Main {
     public static void main(String[] args) {
+
         for (int r : new Randoms(90, 100)) {
             System.out.println("Случайное число: " + r);
             if (r == 100) {
--- src/main/java/ru/netology/Randoms.java
+++ src/main/java/ru/netology/Randoms.java
@@ -1,11 +1,35 @@
 package ru.netology;
 
+import java.util.Iterator;
+import java.util.Random;
+
 public class Randoms implements Iterable<Integer> {
-    protected Random random;
+    protected Random random = new Random();
+    int min;
+    int max;
 
     public Randoms(int min, int max) {
-        //...
+        this.min = min;
+        this.max = max;
     }
 
-    //...
+    public int getRandomNumber() {
+        return random.nextInt(((max - min) + 1)) + min;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return getRandomNumber();
+            }
+        };
+    }
+
 }
